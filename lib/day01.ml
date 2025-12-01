944639fc7367ef0339a0d762b83f8d9df8ceb29d
Yannick Koller <yannick.koller@econ.uzh.ch>
2025-12-01 14:45:18 +0000

    Day 01 - Part 2

--- lib/day01.ml
+++ lib/day01.ml
@@ -7,6 +7,7 @@ let name = "Secret Entrance"
 type direction =
   | L
   | R
+[@@deriving show]
 
 let parse_direction s : direction =
   match s with
@@ -15,16 +16,23 @@ let parse_direction s : direction =
   | _ -> raise Not_found
 ;;
 
-let part1 _ s =
-  let parse_input s : (direction * int) list =
-    let parse s =
-      let length = String.length s in
-      let direction = parse_direction s.[0] in
-      let amount = int_of_string (String.sub s 1 (length - 1)) in
-      (direction, amount)
-    in
-    s |> String.split_on_char '\n' |> List.map parse
+let show_direction d =
+  match d with
+  | L -> "L"
+  | R -> "R"
+;;
+
+let parse_input s : (direction * int) list =
+  let parse s =
+    let length = String.length s in
+    let direction = parse_direction s.[0] in
+    let amount = int_of_string (String.sub s 1 (length - 1)) in
+    (direction, amount)
   in
+  s |> String.split_on_char '\n' |> List.map parse
+;;
+
+let part1 _ s =
   s
   |> parse_input
   |> List.fold_left_map
@@ -43,4 +51,27 @@ let part1 _ s =
   |> List.length
 ;;
 
-let part2 _ _ = failwith "not yet implemented"
+let part2 _ s =
+  s
+  |> parse_input
+  |> List.fold_left_map
+       (fun acc (dir, num) ->
+          let x =
+            match dir with
+            | L -> acc - num
+            | R -> acc + num
+          in
+          let shifted_x = (x + 100) mod 100 in
+          let num_of_times_past_zero =
+            let y = abs (x / 100) in
+            match dir with
+            | L -> (if acc > 0 && (shifted_x = 0 || shifted_x > x) then 1 else 0) + y
+            | R -> y
+          in
+          let new_acc = (shifted_x + 100) mod 100 in
+          (new_acc, num_of_times_past_zero)
+        )
+       50
+  |> snd
+  |> List.fold_left ( + ) 0
+;;
--- test/day01.ml
+++ test/day01.ml
@@ -11,11 +11,24 @@ R14
 L82|}
 ;;
 
+let test_input_2 =
+  {|L68
+L30
+R48
+L5
+R160
+L55
+L1
+L199
+R14
+L82|}
+;;
+
 include Day_test.Make (struct
     module Day = Aoc2025.Day01
 
     let part1_testable = Alcotest.int
-    let part1_test_data = [ (test_input, 3) ]
+    let part1_test_data = [ (test_input, 3) (*1129*)]
     let part2_testable = Alcotest.int
-    let part2_test_data = []
+    let part2_test_data = [ (test_input, 6); (test_input_2, 8) (*6638*) ]
   end)
